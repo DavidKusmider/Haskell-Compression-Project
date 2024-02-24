86219d15d1ff6c6113dc75fd2e1233ac62f89fd6
Romain Barré <rombar999@gmail.com>
2024-02-24 21:08:14 +0100

    first iteration

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,57 @@
 module Main (main) where
 
+import Statistic.Source
+import Statistic.EncodingTree
+import Statistic.Huffman
+
+import Data.Maybe(fromMaybe)
+
+import Data.Typeable(typeOf)
+
+import System.IO
+
+-- Fonction pour écrire le résultat de uncompress dans un fichier
+writeUncompressedToFile :: FilePath -> String -> IO ()
+writeUncompressedToFile filepath list = do
+    putStrLn "ouverture du fichier"
+    handle <- openFile filepath AppendMode  -- Ouvre le fichier pour écriture
+    putStrLn "fichier ouvert"
+    hPutStrLn handle list  -- Écrit le contenu dans le fichier
+    putStrLn "fermeture du fichier"
+    hClose handle  -- Ferme le fichier
+    putStrLn "fichier fermer"
+
+
 main :: IO ()
-main = putStrLn ""
+main = do
+    -- Chaîne de caractères à compresser
+  let inputString = "Hello, world!"
+
+  -- Construction de l'arbre de Huffman à partir de la chaîne de caractères
+  let huffmanTree = tree inputString
+
+  -- Compression de la chaîne de caractères en utilisant l'arbre de Huffman
+  let (maybeCompressedTree, compressedBits) = compress tree inputString
+  -- Gestion des cas où la compression échoue ou où l'arbre de Huffman n'est pas généré avec succès
+  case maybeCompressedTree of
+    Nothing -> putStrLn "La compression a échoué ou l'arbre de Huffman n'a pas été généré avec succès."
+    Just _ -> do
+      -- Affichage des résultats si la compression a réussi
+      putStrLn "Chaîne de caractères d'origine :"
+      putStrLn inputString
+      putStrLn "Arbre de Huffman généré :"
+      print huffmanTree
+      putStrLn "Chaîne de bits compressée :"
+      putStrLn $ concatMap show compressedBits
+      putStrLn $ "message en cours de decrompression"
+      print $ typeOf(maybeCompressedTree)
+      print $ typeOf(compressedBits)
+      let maybeValue = uncompress (maybeCompressedTree, compressedBits)
+      print $ typeOf(maybeValue)
+      putStrLn $ "message decompresse"
+      case maybeValue of
+        Nothing -> putStrLn $ "La valeur est Nothing."
+        Just val -> do
+            writeUncompressedToFile "./output.txt" val -- Écrit le résultat dans le fichier "output.txt"
+            putStrLn $ "Message ecrit"
+      putStrLn "Fin du programme"
--- src/Statistic/EncodingTree.hs
+++ src/Statistic/EncodingTree.hs
@@ -23,31 +23,68 @@ count (EncodingNode cnt _ _) = cnt
 
 -- | Search for symbol in encoding tree
 has :: Eq a => EncodingTree a -> a -> Bool
-_ `has` _ = undefined -- TODO
+(EncodingLeaf _ x) `has` y = x == y
+(EncodingNode _ left right) `has` y = left `has` y || right `has` y
 
 -- | Computes the binary code of symbol using encoding tree
 -- If computation is not possible, returns `Nothing`.
 encode :: Eq a => EncodingTree a -> a -> Maybe [Bit]
-encode _ _ = undefined -- TODO
+encode tree symbol = encodeHelper tree symbol []
+  where
+    encodeHelper :: Eq a => EncodingTree a -> a -> [Bit] -> Maybe [Bit]
+    encodeHelper (EncodingLeaf _ x) y path
+      | x == y    = Just (reverse path)
+      | otherwise = Nothing
+    encodeHelper (EncodingNode _ left right) y path =
+      case (encodeHelper left y (Zero:path), encodeHelper right y (One:path)) of
+        (Just p, _) -> Just p
+        (_, Just p) -> Just p
+        _           -> Nothing
 
 -- | Computes the first symbol from list of bits using encoding tree and also returns the list of bits still to process
 -- If computation is not possible, returns `Nothing`.
 decodeOnce :: EncodingTree a -> [Bit] -> Maybe (a, [Bit])
-decodeOnce _ _ = undefined -- TODO
+decodeOnce tree bits = decodeHelper tree bits []
+  where
+    decodeHelper :: EncodingTree a -> [Bit] -> [Bit] -> Maybe (a, [Bit])
+    decodeHelper (EncodingLeaf _ x) bs acc = Just (x, bs ++ acc)
+    decodeHelper (EncodingNode _ left right) (b:bs) acc =
+      case b of
+        Zero -> decodeHelper left bs (b:acc)
+        One  -> decodeHelper right bs (b:acc)
+    decodeHelper _ [] _ = Nothing
 
 -- | Computes list of symbols from list of bits using encoding tree
 decode :: EncodingTree a -> [Bit] -> Maybe [a]
-decode _ _ = undefined -- TODO
+decode _ [] = Just []
+decode tree bits =
+  case decodeOnce tree bits of
+    Nothing           -> Nothing
+    Just (symbol, bs) -> (symbol :) <$> decode tree bs
+
 
 -- | Mean length of the binary encoding
 meanLength :: EncodingTree a -> Double
-meanLength _ = undefined -- TODO
+meanLength tree = fromIntegral (sumLengths tree) / fromIntegral (count tree)
+  where
+    sumLengths :: EncodingTree a -> Int
+    sumLengths (EncodingLeaf count _)     = count
+    sumLengths (EncodingNode _ left right) = sumLengths left + sumLengths right
+
 
 -- | Compress method using a function generating encoding tree and also returns generated encoding tree
 compress :: Eq a => ([a] -> Maybe (EncodingTree a)) -> [a] -> (Maybe (EncodingTree a), [Bit])
-compress _ _ = undefined -- TODO
+compress treeGenerator input =
+  case treeGenerator input of
+    Nothing   -> (Nothing, [])
+    Just tree -> let encodedInput = mapM (encode tree) input in
+                 case encodedInput of
+                   Nothing -> (Nothing, [])
+                   Just bits -> (Just tree, concat bits)
+
 
 -- | Uncompress method using previously generated encoding tree
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: (Maybe (EncodingTree a), [Bit]) -> Maybe [a]
-uncompress _ = undefined -- TODO
+uncompress (Nothing, _)   = Nothing
+uncompress (Just tree, bits) = decode tree bits
