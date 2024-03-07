ede0b63a8e0aef8b2464fe6eba3ad42497abe667
Davinox <david.kusmider@gmail.com>
2024-03-07 19:13:49 +0100

    uncompressed second checkpoint (almost finished, 1 case left)

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -7,7 +7,7 @@
 module LZ.LZW (compress, uncompress) where
 
 import Data.Maybe (fromMaybe)
-import Data.List (findIndex, find)
+import Data.List (findIndex, find, elemIndex)
 import Data.Char (chr)
 import Data.List (singleton)
 import Debug.Trace (trace)
@@ -49,6 +49,30 @@ compress textToCompress = encode textToCompress ascii "" 256 []
 
 
 
+-- -- | LZW uncompress method
+-- -- If input cannot be uncompressed, returns `Nothing`
+-- uncompress :: [Int] -> String 
+-- uncompress arrayToUnCompress = decode arrayToUnCompress ascii 0 "" 256 "" 
+--     where
+--         decode :: [Int] -> [String] -> Int -> String -> Int -> String -> String 
+--         decode [] dict w _ _ result = result
+--         decode (c:cs) dict w prevString nextCode result
+--             | c >= 0 && c < length dict =
+--                 let currentChar = dict !! c
+--                 in case findIndex (== prevString) dict of
+--                      Just _ ->  trace ("FIND INDEX -> compressed INT : " ++ show c ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ currentChar) $
+--                                 trace ("Dictionary: " ++ show (dict ++ [prevString ++ currentChar])) $
+--                                decode cs dict w currentChar nextCode result
+--                      Nothing -> trace ("NOTHING -> compressed INT : " ++ show c ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ currentChar) $
+--                                 trace ("Dictionary: " ++ show (dict ++ [prevString ++ currentChar])) $
+--                                 decode cs (dict ++ [prevString ++ currentChar]) c currentChar nextCode (result ++ currentChar)
+--             | otherwise =
+--                 trace ("Dictionary: " ++ show (dict ++ [prevString ++ currentChar])) $
+--                 trace ("OTHERWISE -> compressed INT : " ++ show c ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ currentChar) $
+--                 decode cs (dict ++ [prevString ++ currentChar]) c currentChar nextCode (result ++ currentChar)
+--             where 
+--                 currentChar = dict !! c
+
 -- | LZW uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [Int] -> String 
@@ -57,14 +81,34 @@ uncompress arrayToUnCompress = decode arrayToUnCompress ascii 0 "" 256 ""
         decode :: [Int] -> [String] -> Int -> String -> Int -> String -> String 
         decode [] dict w _ _ result = result
         decode (c:cs) dict w prevString nextCode result
-          | c >= 0 && c < length dict = decode cs (dict ++ [prevString ++ currentChar]) c currentChar nextCode (result ++ currentChar)
-          | otherwise = decode cs (dict ++ [prevString ++ currentChar]) c currentChar nextCode (result ++ currentChar)
-          where 
-            currentChar = dict !! c
+            | c >= 0 && c < length dict =
+                let currentChar = dict !! c
+                    combinedChar = currentChar ++ prevString
+                in case findIndex (== combinedChar) dict of
+                    Just _ ->
+                               trace ("\nWITHIN INDEX DICT -> compressed INT : " ++ show c ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ currentChar ++ " | prevIndex : " ++ show prevIndex) $
+                               decode cs dict w currentChar nextCode (result ++ currentChar)
+                    Nothing -> trace ("\nNOTHING -> compressed INT : " ++ show c ++ " | dict length : " ++ show (length dict) ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ currentChar ++ " | char add to dict : " ++ (prevString ++ [head currentChar]) ++ " | prevIndex : " ++ show prevIndex) $
+                               trace ("DictionaryNOTH: " ++ show (dict ++ [prevString ++ [head currentChar]])) $
+                               decode cs (dict ++ [prevString ++ [head currentChar]]) c currentChar nextCode (result ++ currentChar)
+
+            | otherwise =
+                trace ("\nWITHIN OHTERWISE -> compressed INT : " ++ show c ++ " | dict length : " ++ show (length dict) ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ " | char add to dict (prevString + impossible de ajouter currentChar) : " ++ prevString ++ " | prevIndex : " ++ show prevIndex) $
+                trace ("DictionaryOTHER: " ++ show (dict ++ ([prevString ++ "b"]))) $
+                decode cs (dict ++ [prevString]) c (prevString ++ "b") nextCode (result ++ (prevString ++ "b"))
+            where 
+                currentChar = dict !! c
+                prevIndex = getIndexOrZero prevString dict
+
+getIndexOrZero :: Eq a => a -> [a] -> Int 
+getIndexOrZero x xs = case elemIndex x xs of 
+                        Just index -> index + 1
+                        Nothing -> 0
 
 main :: IO ()
 main = do
-    let input = "aaaabaaaa"
+    let input = "ababcbababaaaaaaa"
+    let input = "david davide davide"
     
     let compressed = compress input
     putStrLn $ "Chaîne compressée : " ++ show compressed
