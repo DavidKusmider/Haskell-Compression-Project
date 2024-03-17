11892d43e880f8a544a25a747eb9a6abc242c2ba
Davinox <david.kusmider@gmail.com>
2024-03-17 14:41:58 +0100

    LZW : delete tracks

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -23,16 +23,12 @@ compress textToCompress = encode textToCompress ascii "" 256 []
                   Nothing -> error "Compression: not found in dictionary" 
         encode (c:cs) dict w nextCode result =
           let p = w ++ [c]
-          -- in trace ("dict: " ++ ", p: " ++ show p ++ ", w: " ++ show w ++ ", [c]: " ++ show [c]) $
           in case findIndex (== p) dict of
-                  Just index ->  {-trace ("p (" ++ p ++ ") found in the dictionnary") $-} encode cs dict p nextCode result
+                  Just index -> encode cs dict p nextCode result
                   Nothing ->
-                      -- trace ("p (" ++ p ++ ") not found in the dictionnary") $
                       let code = fromMaybe (error "Compression: not found in dictionary") (findIndex (== w) dict)
                           newDict = dict ++ [p]
-                          -- dict' = dict ++ ["\\" ++ show nextCode]
-                      in --trace ("[code]: " ++ show [code] ++ "dict' : " ++ show newDict) $
-                         encode cs newDict [c] (nextCode + 1) (result ++ [code])
+                      in encode cs newDict [c] (nextCode + 1) (result ++ [code])
 
 -- -- | LZW uncompress method
 uncompress :: [Int] -> String 
@@ -45,26 +41,12 @@ uncompress arrayToUnCompress = decode arrayToUnCompress ascii 0 "" 256 ""
                 let currentChar = dict !! c
                     combinedChar = prevString ++ currentChar
                 in case elemIndex combinedChar dict of
-                    Just index ->
-                               -- trace ("\nWITHIN INDEX DICT -> compressed INT : " ++ show c ++ " | dict length : " ++ show (length dict) ++ " | findIndex : " ++ show index ++ " | combinedChar : " ++ combinedChar ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ currentChar ++ " | char add to dict : " ++ (prevString ++ [head currentChar]) ++ " | prevIndex : " ++ show prevIndex) $
-                               -- trace ("DictionaryJUST: " ++ show (dict ++ [prevString ++ [head currentChar]])) $
-                               decode cs dict w currentChar nextCode (result ++ currentChar)
-                    Nothing -> -- trace ("\nNOTHING -> compressed INT : " ++ show c ++ " | dict length : " ++ show (length dict) ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ currentChar ++ " | char add to dict : " ++ (prevString ++ [head currentChar]) ++ " | prevIndex : " ++ show prevIndex) $
-                               -- trace ("DictionaryNOTH: " ++ show (dict ++ [prevString ++ [head currentChar]])) $
-                               decode cs (dict ++ [prevString ++ [head currentChar]]) c currentChar nextCode (result ++ currentChar)
+                    Just index -> decode cs dict w currentChar nextCode (result ++ currentChar)
+                    Nothing -> decode cs (dict ++ [prevString ++ [head currentChar]]) c currentChar nextCode (result ++ currentChar)
 
             | otherwise =
                 let chaine = prevString ++ [head prevString]
                     newDict = dict ++ [chaine]
-                -- trace ("\nWITHIN OHTERWISE -> compressed INT : " ++ show c ++ " | dict length : " ++ show (length dict) ++ " | prevString : " ++ prevString ++ "  |  current Char : " ++ " | char add to dict (prevString + impossible de ajouter currentChar) : " ++ prevString ++ " | prevIndex : " ++ show prevIndex) $
-                -- trace ("DictionaryOTHER: " ++ show (dict ++ ([prevString ++ prevString]))) $
                 in decode cs newDict c chaine nextCode (result ++ chaine)
             where 
                 currentChar = dict !! c
-                prevIndex = getIndexOrZero prevString dict
-
-
-getIndexOrZero :: Eq a => a -> [a] -> Int 
-getIndexOrZero x xs = case elemIndex x xs of 
-                        Just index -> index + 1
-                        Nothing -> 0
--- src/LZ/main.hs
+++ src/LZ/main.hs
@@ -7,9 +7,9 @@ main = do
     -- let input = "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum."
     -- let input = "HASKELL C'est de la merde !!!"
     -- let input = "le Saut\n de \n ligne a \n l'air de \n fonctionne"
-    -- let input = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    let input = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     -- let input = "ababcbababaaaaaaa"
-    let input = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    -- let input = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     
     let compressed = compress input
 
