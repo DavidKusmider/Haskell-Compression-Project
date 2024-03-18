37e459f523e0c07ca282fa3f317e6424b0b3406b
Davinox <david.kusmider@gmail.com>
2024-03-18 14:58:43 +0100

    dev : fix parse error

--- app/Main.hs
+++ app/Main.hs
@@ -49,7 +49,7 @@ main = do
         "4" -> do
             let input = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             compressLZW input
-            _   -> putStrLn "Choix invalide. Veuillez entrer 1 ou 2."
+        _   -> putStrLn "Choix invalide. Veuillez entrer 1 ou 2."
 
 
 
