22a6b9012feb10c0fea80a89f51d175d94f586af
Davinox <david.kusmider@gmail.com>
2024-03-21 23:20:36 +0100

    dev : refactor LZW without let in

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -8,44 +8,45 @@ module LZ.LZW (compress, uncompress) where
 
 import Data.Maybe (fromMaybe)
 import Data.List (findIndex, elemIndex)
+import Debug.Trace
 import LZ.Dictionaries
 
 
--- | LZW compress method
+-- Compress with dictionary trace
 compress :: String -> [Int]
 compress textToCompress = encode textToCompress ascii "" 256 []
-    where
-        encode :: String -> [String] -> String -> Int -> [Int] -> [Int]
-        encode [] dict w _ result = 
-            case findIndex (== w) dict of
-                  Just index -> result ++ [index] 
-                  Nothing -> error "Compression: not found in dictionary" 
-        encode (c:cs) dict w nextCode result =
-          let p = w ++ [c]
-          in case findIndex (== p) dict of
-                  Just index -> encode cs dict p nextCode result
-                  Nothing ->
-                      let code = fromMaybe (error "Compression: not found in dictionary") (findIndex (== w) dict)
-                          newDict = dict ++ [p]
-                      in encode cs newDict [c] (nextCode + 1) (result ++ [code])
+  where
+    encode :: String -> [String] -> String -> Int -> [Int] -> [Int]
+    encode [] dict w _ result = 
+      trace ("Dictionary: " ++ show dict) $
+        case findIndex (== w) dict of
+          Just index -> result ++ [index]
+          Nothing    -> []
+    encode (c:cs) dict w nextCode result =
+        case findIndex (== p) dict of
+          Just index -> encode cs dict p nextCode result
+          Nothing    -> encode cs newDict [c] (nextCode + 1) (result ++ [code])
+      where
+        p = w ++ [c]
+        code = fromMaybe (error "not found") (findIndex (== w) dict)
+        newDict = dict ++ [p]
 
--- -- | LZW uncompress method
+-- Uncompress with dictionary trace
 uncompress :: [Int] -> String 
 uncompress arrayToUnCompress = decode arrayToUnCompress ascii 0 "" 256 "" 
-    where
-        decode :: [Int] -> [String] -> Int -> String -> Int -> String -> String 
-        decode [] dict w _ _ result = result
-        decode (c:cs) dict w prevString nextCode result
-            | c >= 0 && c < length dict =
-                let currentChar = dict !! c
-                    combinedChar = prevString ++ currentChar
-                in case elemIndex combinedChar dict of
-                    Just index -> decode cs dict w currentChar nextCode (result ++ currentChar)
-                    Nothing -> decode cs (dict ++ [prevString ++ [head currentChar]]) c currentChar nextCode (result ++ currentChar)
-
-            | otherwise =
-                let chaine = prevString ++ [head prevString]
-                    newDict = dict ++ [chaine]
-                in decode cs newDict c chaine nextCode (result ++ chaine)
-            where 
-                currentChar = dict !! c
+  where
+    decode :: [Int] -> [String] -> Int -> String -> Int -> String -> String 
+    decode [] dict w _ _ result = 
+      trace ("Dictionary: " ++ show dict) $
+        result
+    decode (c:cs) dict w prevString nextCode result
+      | c >= 0 && c < length dict = 
+          case elemIndex combinedChar dict of
+            Just index -> decode cs dict w currentChar nextCode (result ++ currentChar)
+            Nothing    -> decode cs (dict ++ [prevString ++ [head currentChar]]) c currentChar nextCode (result ++ currentChar)
+      | otherwise = decode cs newDict c chaine nextCode (result ++ chaine)
+      where 
+        currentChar = dict !! c
+        combinedChar = prevString ++ currentChar
+        chaine = prevString ++ [head prevString]
+        newDict = dict ++ [chaine]
