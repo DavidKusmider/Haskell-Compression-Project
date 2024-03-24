a4ffec043a92beaf9a44ed9565b28e82ff5b8558
Romain Barré <rombar999@gmail.com>
2024-03-24 10:51:07 +0100

    fix: renamed LZ78 function calls

--- app/Main.hs
+++ app/Main.hs
@@ -32,12 +32,12 @@ main = do
         "3" -> do
             let str = "aaaaaaaaaaabbbbbbbbbbbbcccccccccccc"
                 dict = empty
-                compressedLZ78 = compressLZ78 str dict
+                compressedLZ78 = LZ.LZ78.compress str dict
             putStrLn "Chaîne originale :"
             putStrLn str
             putStrLn "Chaîne compressée :"
             print compressedLZ78
-            putStrLn $ "Chaîne decompressée : " ++ uncompressLZ78 compressedLZ78 compressedLZ78
+            putStrLn $ "Chaîne decompressée : " ++ LZ.LZ78.uncompress compressedLZ78 compressedLZ78
 
         "4" -> do
             let input = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
